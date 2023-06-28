5d9753468b67a286710c52367c3797829e902fcd
Pallavinake <98705664+Pallavinake@users.noreply.github.com>
2023-06-28 22:20:29 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
